5bebb62f07cc00654f31d612ec1dbc034f33cd73
TiThuthur <thurthur.petit.py@gmail.com>
2024-07-03 21:21:04 +0200

    essai d'appliqué les static à admin mais semble ne pas être possible

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2^er%0qr_8(0tm9%pa53*qxm*85!++$at_9p8n3pe#vfpc74s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,15 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticflies/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static/"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "listings\static")
-]
